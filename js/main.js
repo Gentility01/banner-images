93ad12aa3aae5767b5440d901129d62d8e0005fc
Gentility01 <mastergentility5@gmail.com>
2023-07-06 21:05:14 +0100

    updated

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-// alert("hello")
-// Get the banner element by its ID
+// Get the body and banner elements
+var body = document.body;
 var banner = document.getElementById('banner');
-    
+
 // Set the paths to the default and scrolled images
 var defaultImage = 'images/cccc.jpg';
 var scrolledImage = 'images/sunnystiches.jpg';
@@ -9,18 +9,47 @@ var scrolledImage = 'images/sunnystiches.jpg';
 // Initialize the last scroll position
 var lastScrollTop = 0;
 
+// Calculate the height of the document
+var documentHeight = Math.max(
+  document.body.scrollHeight,
+  document.documentElement.scrollHeight,
+  document.body.offsetHeight,
+  document.documentElement.offsetHeight,
+  document.body.clientHeight,
+  document.documentElement.clientHeight
+);
+
+// Calculate the height at which the second image should start appearing
+var startScrollHeight = documentHeight / 2;
+
+// Calculate the height at which the second image should be fully visible
+var endScrollHeight = documentHeight;
+
 // Add a scroll event listener to the window
 window.addEventListener('scroll', function() {
   // Get the current scroll position
   var st = window.pageYOffset || document.documentElement.scrollTop;
   
-  // Check if the current scroll position is greater than the last scroll position
+  // Calculate the opacity based on the scroll position
+  var opacity = (st - startScrollHeight) / (endScrollHeight - startScrollHeight);
+  
+  // Limit the opacity value between 0 and 1
+  opacity = Math.min(Math.max(opacity, 0), 1);
+  
+  // Calculate the translateY value for the banner
+  var translateY = Math.min(st - startScrollHeight, 0);
+  
+  // Set the background image and opacity
   if (st > lastScrollTop) {
-    // Set the background image to the scrolled image
-    banner.style.backgroundImage = 'url(' + scrolledImage + ')';
+    // Scrolling down
+    body.style.backgroundImage = 'url(' + defaultImage + ')';
+    banner.style.opacity = 1 - opacity;
+    banner.style.transform = 'translateY(' + translateY + 'px)';
   } else {
-    // Set the background image to the default image
-    banner.style.backgroundImage = 'url(' + defaultImage + ')';
+    // Scrolling up
+    body.style.backgroundImage = 'url(' + scrolledImage + ')';
+    banner.style.opacity = opacity;
+    banner.style.transform = 'translateY(' + translateY + 'px)';
   }
   
   // Update the last scroll position with the current scroll position
---
+++ test.py
@@ -0,0 +1,7 @@
+print(bool(" ")+4)
+print(bool)
+
+a = 10
+b = 20
+res = a if a>b else b
+print("great", res)
